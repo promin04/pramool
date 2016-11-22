de5f3bf92b2c3501522ad6c83ce4770e0ef90f57
Prommin <crazyfox_pr@hotmail.com>
2016-11-22 20:54:30 +0700

    fixed creater

--- public/bin/bundle.js
+++ public/bin/bundle.js
@@ -15147,11 +15147,11 @@
 	  angular.module('addProduct')
 	    .controller( 'sidebarReviewController' , [ '$scope' , function ( $scope ) {
 	      var that = this;
-	      console.log($scope , '55555555555+');
+	      console.log($scope.user,'555');
 	      this.name = $scope.$parent.product.input.name;
 	      this.bidEnd = ($scope.$parent.product.input.time.days * 24 * 60 * 60 * 1000) + ($scope.$parent.product.input.time.hours * 60 * 60 * 1000);
 	      this.description = $scope.$parent.product.input.description;
-	
+	      this.creator = { username : $scope.user };
 	      this.bider = [{
 	        price : $scope.$parent.product.input.price,
 	        name : $scope.user,
